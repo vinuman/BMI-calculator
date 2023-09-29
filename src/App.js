a6730010ef0c0b3121823588e4db10e03a14ac57
Vinu Cyril <vinucyril@gmail.com>
2023-09-29 14:33:57 +0530

    top section left

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <div className="App font-inter max-w-[1536px] mx-auto">
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
